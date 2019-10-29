bba638d28037a5af23eaf6b59d571d4e8683cc86
Davide Cazzetta <d.cazzetta@campus.unimib.it>
2019-10-29 11:39:28 +0100

    First commit

---
+++ quickstart/src/main/java/DavideCazzetta/quickstart/App.java
@@ -0,0 +1,13 @@
+package DavideCazzetta.quickstart;
+
+import com.google.gson.Gson;
+
+public class App
+{
+    public static void main( String[] args )
+    {
+        Gson gson = new Gson();
+        System.out.println(gson.toJson("Hello World!") );
+    }
+}
+
